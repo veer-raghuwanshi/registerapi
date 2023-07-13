659e359a341260f590d80a75663e83310b63fea7
Veerendra Raghuwanshi <68636328+veer-raghuwanshi@users.noreply.github.com>
2023-07-13 09:11:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/uploads',express.static('uploads'));
+//app.use('/uploads',express.static('uploads'));
+      
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
