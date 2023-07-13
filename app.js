0ee4a52d0536663c3c9ac442851b111a13f21cc5
Veerendra Raghuwanshi <68636328+veer-raghuwanshi@users.noreply.github.com>
2023-07-13 09:36:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/uploads',express.static('uploads'));
-      app.use('/custom-url', express.static('uploads'));
+app.use('/uploads',express.static('uploads'));
 
 
 app.use('/', indexRouter);
