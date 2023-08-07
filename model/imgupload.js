3f215f0e39002063488e9a2fa35def9fd21ce9c6
Veerendra Raghuwanshi <68636328+veer-raghuwanshi@users.noreply.github.com>
2023-08-07 13:47:32 +0530

    Update imgupload.js

--- model/imgupload.js
+++ model/imgupload.js
@@ -1,13 +1,16 @@
 var multer = require('multer')
 
 var storage_engine  =  multer.diskStorage({
-   destination: (req,file,cb)=>{
-    cb(null,'./uploads')
+   destination: './public/images',
+   filename: (req,file,cb)=>{
+    // cb(null,'./uploads')
+           cb(null,Date.now() + '_' + file.originalname)
+
    },
-   filename:(req, file, cb) => {
-       cb(null,Date.now() + '_' + file.originalname)
+//    filename:(req, file, cb) => {
+//        cb(null,Date.now() + '_' + file.originalname)
    
-   }
+//    }
 })
 let imgupload = multer({storage:storage_engine})
 module.exports = imgupload
